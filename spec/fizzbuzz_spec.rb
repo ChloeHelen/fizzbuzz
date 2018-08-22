61d3da7a4fd899f2a94bb45b7f4f3183850fdf93
Chloe Mingay <cmingay@fastmail.fm>
2018-08-22 13:17:46 +0000

    adds test for multiple of 3, program passes test

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,6 +1,13 @@
 require './lib/fizzbuzz'
+
 describe 'fizzbuzz' do
    it 'return "fizz" when passed 3' do
        expect(fizzbuzz(3)).to eq 'fizz'
     end
+    
+    it 'returns "fizz" when passed a multiple of 3' do
+        expect(fizzbuzz(6)).to eq 'fizz'
+    end
 end
+
+    
