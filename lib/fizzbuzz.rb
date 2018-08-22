828b3e1d86504fe9ef141d8f9ecf646bd15a08cb
Chloe Mingay <cmingay@fastmail.fm>
2018-08-22 13:54:12 +0000

    program only accepts integers greater than 0

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,11 +1,15 @@
 def fizzbuzz(number)
-    if number % 3 == 0 && number % 5 == 0
-        'fizzbuzz'
-    elsif number % 3 == 0
-        'fizz'
-    elsif number % 5 == 0
-        'buzz'
+    if number.class == Integer && number > 0
+        if number % 3 == 0 && number % 5 == 0
+            'fizzbuzz'
+        elsif number % 3 == 0
+            'fizz'
+        elsif number % 5 == 0
+            'buzz'
+        else
+            number
+        end
     else
-        number
+        'Invalid input, number must be a a whole number greater than 0'
     end
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -19,6 +19,12 @@ describe 'fizzbuzz' do
         it 'returns "number" when not pass a multiple of both 3 or 5' do
         expect(fizzbuzz(4)).to eq 4
     end
+    
+        it 'requires number passed to be a whole number greater  than 0' do
+            expect(fizzbuzz(4.1)).to eq 'Invalid input, number must be a a whole number greater  than 0'
+            expect(fizzbuzz(-2)).to 'Invalid input, number must be a a whole number greater  than 0'
+            expect(fizzbuzz('word')).to 'Invalid input, number must be a a whole number greater  than 0'
 end
+
 
     
