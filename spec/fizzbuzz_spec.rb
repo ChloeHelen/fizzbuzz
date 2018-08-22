9bd4d70aede2962f2c7d6cd7123e30090e153473
Chloe Mingay <cmingay@fastmail.fm>
2018-08-22 13:23:53 +0000

    adds test for multiple of 3 and 5, program passes test

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -12,6 +12,10 @@ describe 'fizzbuzz' do
    it 'returns "buzz" when pass a multiple of 5' do
        expect(fizzbuzz(5)).to eq 'buzz'
     end
+    
+    it 'returns "fizzbuzz" when pass a multiple of both 3 and 5' do
+        expect(fizzbuzz(15)).to eq 'fizzbuzz'
+    end
 end
 
     
